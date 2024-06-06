af1998a984ac6d0fdfeaff1e80feb5405a40a34a
joaorobra <joaorobra@gmail.com>
2024-06-06 20:12:51 -0300

    alterações

--- bootstrap-vue/src/main.js
+++ bootstrap-vue/src/main.js
@@ -1,4 +1,13 @@
+import Vue from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
+import bootstrap from './bootstrap';
+
+Vue.config.productionTip = false
+Vue.use(bootstrap);
+
+new Vue({
+    render: h => h(app),})
 
 createApp(App).mount('#app')
+
